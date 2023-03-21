fccfb12eedc49097aec51998d177e102bebfaa44
Broderick Westrope <broderickwestrope@gmail.com>
2023-03-21 14:29:12 +1100

    New Build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/First-Vue-Project/',
+    base: '/Vue-Task-Manager/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
